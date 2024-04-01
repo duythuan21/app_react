2eddc6c227fa639867ac28345c8d479f109e74c5
Duy Thuan <thuan.work2104@gmail.com>
2024-04-01 13:44:31 +0700

    update code

---
+++ components/MenuComponent.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { FlatList } from 'react-native';
+import { ListItem, Avatar } from 'react-native-elements';
+//import Dishdetail from './DishdetailComponent';
+import { DISHES } from '../shared/dishes';
+
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //selectedDish: null,
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      // <View style={{ flex: 1 }}>
+        <FlatList
+          data={this.state.dishes}
+          renderItem={({ item, index }) => this.renderMenuItem(item, index)}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      /* <Dishdetail dish={this.state.selectedDish} /> */
+      //</View>
+    );
+  }
+  renderMenuItem(item, index) {
+    const { navigate } = this.props.navigation;
+    return (
+      <ListItem key={index} onPress={() => navigate('Dishdetail', { dishId: item.id })}>
+        <Avatar source={require('./images/uthappizza.png')}/>
+        <ListItem.Content>
+          <ListItem.Title>{item.name}</ListItem.Title>
+          <ListItem.Subtitle>{item.description}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    );
+  }
+  // onDishSelect(item) {
+  //   this.setState({ selectedDish: item });
+  // }
+}
+
+export default Menu;
